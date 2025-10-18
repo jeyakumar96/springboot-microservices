ccc5a8754441a65e474038a5ea8d739509d53ae8
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-10-18 19:42:31 +0530

    create students returns List

--- springboot-rest-api/src/main/java/com/example/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/example/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.example.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -17,4 +20,15 @@ public class StudentController {
         );
         return student;
     }
-}
+
+    // http://localhost:8080/students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Jeyakumar", "Moorthy"));
+        students.add(new Student(2, "Vinothkumar", "Moorthy"));
+        students.add(new Student(3, "Usha", "Nandhini"));
+        students.add(new Student(4, "Kavitha", "Moorthy"));
+        return students;
+        }
+    }
