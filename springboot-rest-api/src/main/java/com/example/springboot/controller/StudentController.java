cddde69d60d854eaf03f2169e648e7acb38528b6
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-10-19 10:11:34 +0530

    Create REST API PostMapping & RequestBody

--- springboot-rest-api/src/main/java/com/example/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/example/springboot/controller/StudentController.java
@@ -1,11 +1,8 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.bean.Student;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +49,18 @@ public class StudentController {
                                           @RequestParam String firstName,
                                           @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+    }
+
+    // Spring BOOT REST API to handle HTTP POST Request
+    // @PostMapping and @RequestBody
+    // http://localhost:8080/students/create
+    @PostMapping("/students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 
 }
