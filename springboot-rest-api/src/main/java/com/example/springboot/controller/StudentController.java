88a072be0209b4e1ffb809be86b8ca261f010e46
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-10-19 09:57:42 +0530

    Create REST API with RequestParam

--- springboot-rest-api/src/main/java/com/example/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/example/springboot/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.example.springboot.bean.Student;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -42,6 +43,15 @@ public class StudentController {
                                        @PathVariable("first-name") String firstName,
                                        @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    // Spring BOOT REST API with Request Param
+    // http://localhost:8080/students/query?id=1&firstName=Jeyakumar&lastName=Moorthy
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
     }
 
 }
