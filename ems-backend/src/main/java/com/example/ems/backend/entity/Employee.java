0a5a54e497f1e8349074f33375153a326cefcbb3
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-11-10 10:00:30 +0530

    Build CRUD REST APIs for Employee Management App

---
+++ ems-backend/src/main/java/com/example/ems/backend/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.example.ems.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", unique = true)
+    private String email;
+}
